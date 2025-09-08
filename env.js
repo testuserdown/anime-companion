7b6d21a633c87256dc7e854c45429c9c339309ac
testuserdown <exefiledev@gmail.com>
2025-09-09 00:08:52 +0500

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.__ENV = {
-    SITE_TITLE: "Aura Intelligence",
+    SITE_TITLE: "Aura Intelligence 2",
     X_HANDLE: "https://x.com/new",
     SITE_DESC: "A gentle app to reflect your mood with soothing anime vibes"
 };
