e89c5056cd605709d2019a6245d997468431641b
testuserdown <exefiledev@gmail.com>
2025-09-08 23:12:48 +0500

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.__ENV = {
-    SITE_TITLE: "Aure bot",
+    SITE_TITLE: "Aure Intelligence",
     X_HANDLE: "https://x.com/new",
     SITE_DESC: "A gentle app to reflect your mood with soothing anime vibes"
 };
